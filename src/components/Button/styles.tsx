2d897da36cebf7fd70ec63afed1a7128eaed5dfe
Ariel V Lima <arielflima@gmail.com>
2020-05-28 17:30:38 -0300

    creating Unform to SignUp form

--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Container = styled.button`
-  button {
     background: #ff9000;
     height: 56px;
     border-radius: 10px;
