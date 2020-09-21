66298695cacdc3fa89d9e4e0a1a1fc3fa542134c
Ariel V Lima <arielflima@gmail.com>
2020-09-21 14:33:24 -0300

    testing login component

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,35 @@
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+describe('SignIn Page', () => {
+  it('Should be able to sign in', () => {
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    fireEvent.change(emailField, {
+      target: { value: 'joaoninguem@exemplo.com' },
+    });
+    fireEvent.change(passwordField, {
+      target: { value: '123456' },
+    });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
+  });
+});
